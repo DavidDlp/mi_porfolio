09f7bed366a05f85e64fe02f39c5d839ad62e090
David Dlp <dlpdavid17@gmail.com>
2021-12-19 17:18:21 +0100

    create workData

---
+++ src/workData.js
@@ -0,0 +1,23 @@
+const workData = [
+    {
+        title: "Lucky",
+        image: "#",
+        description: "Proyecto grupal realizado con React & NodeJs, aplicación de adopción de mascotas. ",
+        link: "",
+    },
+    {
+        title: "Pokedex",
+        image: "#",
+        description: "Proyecto realizado con React, sobre la temática pokemon.",
+        link: "",
+    },
+    {
+        title: "Rick&Morty",
+        image: "#",
+        description: "Proyecto realizado con Angular y Bootstrap, sobre la serie de animacion Rick and Morty.",
+        link: "",
+    },
+    
+];
+
+export default workData;
