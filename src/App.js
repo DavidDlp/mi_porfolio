5000fc0356ded609c59eac0048035fa1cfca5d81
David Dlp <dlpdavid17@gmail.com>
2021-12-21 16:36:45 +0100

    normalize and new workdata

--- src/App.js
+++ src/App.js
@@ -4,10 +4,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from './components/core/Footer';
 import Home from './components/core/Home';
 import MyWork from './components/pages/MyWork';
+import { createTheme, MuiThemeProvider } from '@material-ui/core';
+
+const theme = createTheme({
+  palette: {
+    background: {main: "#333333"},
+    primary: {main: "#246EB9"},
+    secondary: {main: "#A69658" },
+    third: {main: "#FDFFFC"},
+    special: {main: "#75DBCD"}
+  }
+})
 
 function App() {
 
   return (
+    <MuiThemeProvider theme={theme}>
     <div>
       <BrowserRouter>
         <Routes>
@@ -19,8 +31,8 @@ function App() {
         <Footer title="Contact Me" dark={false}/>
       </BrowserRouter>
     </div>
+    </MuiThemeProvider>
   );
 }
-
 
 export default App;
--- src/workData.js
+++ src/workData.js
@@ -1,21 +1,22 @@
 const workData = [
     {
         title: "Lucky",
+        subtitle: "LuckyApi",
         image: "#",
         description: "Proyecto grupal realizado con React & NodeJs, aplicación de adopción de mascotas. ",
-        link: "",
+        link: "https://github.com/DavidDlp/Project-lucky",
     },
     {
-        title: "Pokedex",
+        title: "Todo List",
         image: "#",
-        description: "Proyecto realizado con React, sobre la temática pokemon.",
-        link: "",
+        description: "Proyecto Todo list realizado con JavaScripts.",
+        link: "https://github.com/DavidDlp/js-todolist",
     },
     {
         title: "Rick&Morty",
         image: "#",
         description: "Proyecto realizado con Angular y Bootstrap, sobre la serie de animacion Rick and Morty.",
-        link: "",
+        link: "https://github.com/DavidDlp/rick-morty-angular",
     },
     
 ];
