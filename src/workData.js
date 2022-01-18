ab65f5c1530e36f566d38cc70bb13a1ee12daf7b
David Dlp <dlpdavid17@gmail.com>
2022-01-18 12:07:24 +0100

    new project in my work

--- src/workData.js
+++ src/workData.js
@@ -1,16 +1,28 @@
 import rickmorty from "./assets/images/rick_y_morty.jpg";
 import todolist from "./assets/images/todolist.jpg";
 import lucky from "./assets/images/lucky.jpg";
-import pokemon from "./assets/images/pokemon.jpg"
+import pokemon from "./assets/images/pokemon.jpg";
+import diving from "./assets/images/buceo.jpg"
 
 
 const workData = [
     {
         title: "Lucky",
-        subtitle: "LuckyApi",
         image: lucky ,
-        description: "Proyecto grupal realizado con React & NodeJs, aplicación de adopción de mascotas, versión mobile. ",
+        description: "Proyecto grupal realizado con React , aplicación de adopción de mascotas, versión mobile. ",
         link: "https://project-lucky.netlify.app/",
+    },
+    {
+        title: "LuckyApi",
+        image: lucky ,
+        description: "Api del Proyecto grupal realizado con NodeJs, aplicación de adopción de mascotas, versión mobile. ",
+        link: "https://github.com/DavidDlp/project-lucky-api",
+    },
+    {
+        title: "Diving paradise",
+        image: diving,
+        description: "Landing page, realizado con React & Material UI. Elige tu destino favorito para disfrutar de tu hobby.",
+        link: "https://divingparadise.netlify.app/",
     },
     {
         title: "Todo List",
