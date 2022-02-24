5f884bc5a39df8ca86c141294af9da66003d4d0c
David Dlp <dlpdavid17@gmail.com>
2022-02-24 11:08:17 +0100

    new organize Tecnologies

--- src/components/pages/Tecnologies.js
+++ src/components/pages/Tecnologies.js
@@ -1,66 +1,67 @@
-import React from 'react';
-import { Box, makeStyles } from '@material-ui/core';
-import { IoLogoAngular, 
-        IoLogoCss3, 
-        IoLogoHtml5, 
-        IoLogoJavascript,
-    } from 'react-icons/io';
-import { FaNode, FaPhp, FaReact } from 'react-icons/fa';
-import { SiMongodb, SiMysql } from 'react-icons/si';
+import React from "react";
+import { Box, makeStyles } from "@material-ui/core";
+import {
+  IoLogoAngular,
+  IoLogoCss3,
+  IoLogoHtml5,
+  IoLogoJavascript,
+} from "react-icons/io";
+import { FaJava, FaNode, FaReact } from "react-icons/fa";
+import { SiMongodb, SiMysql } from "react-icons/si";
 
-const Tecnologies = () =>{
-    const classes = useStyles();
+const Tecnologies = () => {
+  const classes = useStyles();
 
-    return (
-        <Box className={classes.container}>
-        <div className={classes.containerlogo}>
-            <IoLogoHtml5 className={classes.logo}/>
-        </div>
-        <div className={classes.containerlogo}>
-            <IoLogoCss3 className={classes.logo}/>
-        </div>
-        <div className={classes.containerlogo}>
-            <IoLogoJavascript className={classes.logo}/>
-        </div>
-        <div className={classes.containerlogo}>
-            <IoLogoAngular className={classes.logo}/>
-        </div >
-        <div className={classes.containerlogo}>
-            <FaReact className={classes.logo}/>
-        </div>
-        <div className={classes.containerlogo}>
-            <FaNode className={classes.logo}/>
-        </div>
-        <div className={classes.containerlogo}>
-            <FaPhp className={classes.logo}/>
-        </div>
-        <div className={classes.containerlogo}>
-            <SiMysql className={classes.logo}/>
-        </div>
-        <div className={classes.containerlogo}>
-            <SiMongodb className={classes.logo}/>
-        </div>
-        </Box>
-    )
+  return (
+    <Box className={classes.container}>
+      <div className={classes.containerlogo}>
+        <IoLogoHtml5 className={classes.logo} />
+      </div>
+      <div className={classes.containerlogo}>
+        <IoLogoCss3 className={classes.logo} />
+      </div>
+      <div className={classes.containerlogo}>
+        <IoLogoJavascript className={classes.logo} />
+      </div>
+      <div className={classes.containerlogo}>
+        <IoLogoAngular className={classes.logo} />
+      </div>
+      <div className={classes.containerlogo}>
+        <FaReact className={classes.logo} />
+      </div>
+      <div className={classes.containerlogo}>
+        <FaNode className={classes.logo} />
+      </div>
+      <div className={classes.containerlogo}>
+        <FaJava className={classes.logo} />
+      </div>
+      <div className={classes.containerlogo}>
+        <SiMysql className={classes.logo} />
+      </div>
+      <div className={classes.containerlogo}>
+        <SiMongodb className={classes.logo} />
+      </div>
+    </Box>
+  );
 };
 
 const useStyles = makeStyles((theme) => ({
-    container: {
-        // border: '1px solid grey',
-        // borderRadius: 16,
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        textAlign: "center",
-        flexWrap: "wrap",
-        margin: theme.spacing(3),
-    },
-    containerlogo:{
-        margin: "0 auto",
-    },
-    logo:{
-        fontSize: "3rem"
-    },
-}))
+  container: {
+    // border: '1px solid grey',
+    // borderRadius: 16,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    textAlign: "center",
+    flexWrap: "wrap",
+    margin: theme.spacing(3),
+  },
+  containerlogo: {
+    margin: "0 auto",
+  },
+  logo: {
+    fontSize: "3rem",
+  },
+}));
 
-export default Tecnologies
+export default Tecnologies;
